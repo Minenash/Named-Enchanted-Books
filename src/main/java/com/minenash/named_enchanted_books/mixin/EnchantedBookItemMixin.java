a4d5993c724cf464ec3fa9f3cb7570803c056dc2
minenash <minenash@protonmail.com>
2021-05-25 01:36:45 -0400

    wrote mod

---
+++ src/main/java/com/minenash/named_enchanted_books/mixin/EnchantedBookItemMixin.java
@@ -0,0 +1,46 @@
+package com.minenash.named_enchanted_books.mixin;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(EnchantedBookItem.class)
+public class EnchantedBookItemMixin extends Item {
+
+    public EnchantedBookItemMixin(Settings settings) { super(settings); }
+
+    @Override
+    public Text getName(ItemStack stack) {
+
+        ListTag list = EnchantedBookItem.getEnchantmentTag(stack);
+        if (list.isEmpty())
+            return super.getName(stack);
+
+        CompoundTag tag = list.getCompound(0);
+        Enchantment enchantment = Registry.ENCHANTMENT.get(Identifier.tryParse(tag.getString("id")));
+
+        if (enchantment != null) {
+            MutableText text = new LiteralText("Book of ").append(new TranslatableText(enchantment.getTranslationKey()));
+            int level = tag.getInt("lvl");
+
+            if (level != 1 || enchantment.getMaxLevel() != 1)
+                text.append(" ").append(new TranslatableText("enchantment.level." + level));
+
+            if (list.size() > 1)
+                text.append(" ...");
+
+            return text;
+        }
+        return super.getName(stack);
+    }
+}
